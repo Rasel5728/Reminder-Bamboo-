9e9d4fba595ee6522f3f3c7c6bb22e1c3c162ca3
Rasel5728 <muhammadrasel5792@gmail.com>
2026-08-28 03:18:02 +0600

    feat(Add HomePage UI Design and logic)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -43,11 +43,81 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
-
-
+  //UI design
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("TASKS")),
+      body: tasks.isEmpty
+          ? const Center(
+              child: Text(
+                "No Task Add,Click \n+ Button To Add New Task",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 6),
+                  child: ListTile(
+                    //checkbox
+                    leading: Checkbox(
+                      value: task.isDone,
+                     onChanged: (value)=>toggleDone(index),
+                     ),
+
+                     //Title
+                     title: Text(
+                      task.title,
+                      style: TextStyle(
+                        //strikethough
+                        decoration: task.isDone?TextDecoration.lineThrough:TextDecoration.none,
+                        fontWeight: FontWeight.bold,
+                      ),
+                     ),
+
+                     //SubTitle
+                     subtitle: Text(
+                      "${task.description}\n"
+                      "${task.dateTime.day}/${task.dateTime.month}/${task.dateTime.year} "
+                      "${TimeOfDay.fromDateTime(task.dateTime).format(context)}",
+                     ),
+                     isThreeLine: true,
+
+                     //Priority Idicator and Delete Button
+
+                     trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //High Priority indicate red dot
+                        if(task.isHighPriority)
+                        const Padding(
+                          padding: EdgeInsets.only(right: 8),
+                          child: Icon(Icons.priority_high,color: Colors.red,),
+                        ),
+                        IconButton(
+                          onPressed: ()=> deleteTask(index), 
+                          icon: Icon(Icons.delete_outline),
+                          ),
+                      ],
+                     ),
+
+
+                  ),
+                );
+              },
+            ),
+
+            //Add Button
+            floatingActionButton: FloatingActionButton(onPressed: goToAddTaskPage,
+            child: const Icon(Icons.add),
+            
+            ),
+    );
   }
 }
