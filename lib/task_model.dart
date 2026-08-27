b9ba441aeb1121ac4f3b0f3b6477f6cf960868e4
Rasel5728 <muhammadrasel5792@gmail.com>
2026-08-28 03:42:39 +0600

    feat(Add Hive auto runner in task_model)

--- lib/task_model.dart
+++ lib/task_model.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class Task {
+part 'task_model.g.dart'; // build_runner autoGenarate
+
+@HiveType(typeId: 0) // typeID
+class Task extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String description;
+
+  @HiveField(2)
   DateTime dateTime;
+
+  @HiveField(3)
   bool isHighPriority;
+
+  @HiveField(4)
   bool isDone;
 
   Task({
@@ -12,7 +24,6 @@ class Task {
     required this.description,
     required this.dateTime,
     required this.isHighPriority,
-    this.isDone=false,
-
+    this.isDone = false,
   });
 }
